eefd1b307a2a8be30edbbaed7f8baa05d5a017b7
HugoBrunetENI <hugo.brunet2022@campus-eni.fr>
2023-04-04 10:27:35 +0200

    BO ArticleVendu, Categorie, Retrait

---
+++ src/fr/eni/encheres/bo/ArticleVendu.java
@@ -0,0 +1,89 @@
+package fr.eni.encheres.bo;
+
+import java.sql.Date;
+
+public class ArticleVendu {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private int prixInitial;
+	private int prixVente;
+	private Utilisateur achete;
+	private Utilisateur vend;
+	private Categorie categorie;
+	
+	public ArticleVendu() {}
+	
+	public ArticleVendu(String nomArticle, String description, Date dateDebutEncheres, Date dateFinEncheres,
+			Utilisateur vend, Categorie categorie) {
+		this.setNomArticle(nomArticle);
+		this.setDescription(description);
+		this.setDateDebutEncheres(dateDebutEncheres);
+		this.setDateFinEncheres(dateFinEncheres);
+		this.setVend(vend);
+		this.setCategorie(categorie);
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+	public void setDateDebutEncheres(Date dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+	public Date getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+	public void setDateFinEncheres(Date dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+	public int getPrixInitial() {
+		return prixInitial;
+	}
+	public void setPrixInitial(int prixInitial) {
+		this.prixInitial = prixInitial;
+	}
+	public int getPrixVente() {
+		return prixVente;
+	}
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+	public Utilisateur getAchete() {
+		return achete;
+	}
+	public void setAchete(Utilisateur achete) {
+		this.achete = achete;
+	}
+	public Utilisateur getVend() {
+		return vend;
+	}
+	public void setVend(Utilisateur vend) {
+		this.vend = vend;
+	}
+	public Categorie getCategorie() {
+		return categorie;
+	}
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+}
