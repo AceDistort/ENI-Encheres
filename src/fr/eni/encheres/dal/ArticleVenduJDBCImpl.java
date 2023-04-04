88b0cb6a49ad25295f05181b1d779901fc378d8b
HugoBrunetENI <hugo.brunet2022@campus-eni.fr>
2023-04-04 11:56:29 +0200

    Mise en place DAL Gestion des enchères

--- src/fr/eni/encheres/dal/ArticleVenduJDBCImpl.java
+++ src/fr/eni/encheres/dal/ArticleVenduJDBCImpl.java
@@ -3,6 +3,8 @@ package fr.eni.encheres.dal;
 import fr.eni.encheres.bo.ArticleVendu;
 
 public class ArticleVenduJDBCImpl implements ArticleVenduDAO {
+	
+	private static final String CREER_VENTE_ARTICLE = "INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,no_utilisateur,no_categorie) VALUES (?,?,?,?,?,?,?);";
 
 	/**
 	 * {@inheritDoc}
