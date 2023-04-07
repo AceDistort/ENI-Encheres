eeceef45abd7d3e25af0c1962bb1c2a738a4e2c0
HugoBrunetENI <hugo.brunet2022@campus-eni.fr>
2023-04-07 09:32:49 +0200

    Creation Servlet nouvelle vente

---
+++ src/fr/eni/encheres/servlets/NouvelleVenteServlet.java
@@ -0,0 +1,47 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NouvelleVenteServlet
+ */
+@WebServlet("/NouvelleVenteServlet")
+public class NouvelleVenteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NouvelleVenteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//Redirection vers page connexion si utilisateur non connecté
+		if(request.getSession().getAttribute("sessionUtilisateur") == null) {
+			((HttpServletResponse) response).sendRedirect("connexion");
+		} else {
+			
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
