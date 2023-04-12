b13365f05c11725522b7ed5a1f6f338124b2eba8
GuillaumeVincent <guillaume.vincent12022@campus-eni.fr>
2023-04-12 10:24:56 +0200

    Ajout DAL & BLL Enchere + CodeErreur***

--- src/fr/eni/encheres/dal/EnchereDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/EnchereDAOJdbcImpl.java
@@ -1,35 +1,82 @@
 package fr.eni.encheres.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
+import fr.eni.encheres.bo.BusinessException;
 import fr.eni.encheres.bo.Enchere;
 
 public class EnchereDAOJdbcImpl implements EnchereDAO {
 	//Attributs d'instances
-	private static final String CREER="";
-	private static final String MODIFIER="";
-	private static final String SUPPRIMER="";
-	private static final String LISTER_TOUTES="";
+	private static final String CREER="INSERT INTO ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere) VALUES (?,?,?,?);";
+	private static final String MODIFIER="UPDATE ENCHERES SET montant_enchere=? WHERE no_utilisateur=? AND no_article=?;";
+	private static final String SUPPRIMER="DELETE FROM ENCHERE WHERE no_utilisateur=? AND no_article=?;";
 
 	@Override
-	public void creerEnchere(Enchere enchere) {
+	public void creerEnchere(Enchere enchere) throws BusinessException {
+		if (enchere == null) {
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.OBJET_NULL_AJOUT_ENCHERE);
+			throw businessException;
+		}
+		try(Connection cnx = ConnectionProvider.getConnection())
+		{
+			PreparedStatement pstmt = cnx.prepareStatement(CREER);
+			pstmt.setInt(1, enchere.getUtilisateur().getNoUtilisateur());
+			pstmt.setInt(2, enchere.getArticle().getNoArticle());
+			pstmt.setDate(3, enchere.getDateEnchere());
+			pstmt.setInt(4, enchere.getMontantEnchere());
+			pstmt.executeUpdate();
+			
+		} catch(Exception e) {
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.AUTRE_ERREUR_AJOUT_ENCHERE);
+			throw businessException;
+		}
+	}
+
+	@Override
+	public void modifierEnchere(Enchere enchere) throws BusinessException {
+		if (enchere == null) {
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.OBJET_NULL_MODIF_ENCHERE);
+			throw businessException;
+		}
 		
+		try(Connection cnx = ConnectionProvider.getConnection())
+		{
+			PreparedStatement pstmt = cnx.prepareStatement(MODIFIER);
+			pstmt.setInt(1, enchere.getMontantEnchere());
+			pstmt.setInt(2, enchere.getUtilisateur().getNoUtilisateur());
+			pstmt.setInt(3, enchere.getArticle().getNoArticle());
+			pstmt.executeUpdate();
+			
+		} catch(Exception e) {
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.AUTRE_ERREUR_MODIF_ENCHERE);
+			throw businessException;
+		}
 	}
 
 	@Override
-	public void modifierEnchere(Enchere enchere) {
+	public void supprimerEnchere(Enchere enchere) throws BusinessException {
+		if (enchere == null) {
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.OBJET_NULL_SUPP_ENCHERE);
+			throw businessException;
+		}
 		
+		try(Connection cnx = ConnectionProvider.getConnection()){
+			PreparedStatement pstmt = cnx.prepareStatement(SUPPRIMER);
+			pstmt.setInt(1, enchere.getUtilisateur().getNoUtilisateur());
+			pstmt.setInt(2, enchere.getArticle().getNoArticle());
+			pstmt.executeUpdate();
+			
+		} catch(Exception e) {
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.AUTRE_ERREUR_SUPP_ENCHERE);
+			throw businessException;
+		}
 	}
-
-	@Override
-	public void supprimerEnchere(Enchere enchere) {
-		
-	}
-
-	@Override
-	public List<Enchere> listerEncheres() {
-		List<Enchere> encheres;
-		return null;
-	}
-
 }
